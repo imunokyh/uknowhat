4f051cbb85ad215a33d958cbc55a1272f0484a6d
hyounggo <hyounggo@crossent.com>
2021-10-10 21:01:47 +0900

    config update

--- src/main/java/kr/or/uknowhat/api/framework/config/WebConfiguration.java
+++ src/main/java/kr/or/uknowhat/api/framework/config/WebConfiguration.java
@@ -1,41 +1,43 @@
 package kr.or.uknowhat.api.framework.config;
 
-import java.io.IOException;
 
-//import org.springframework.boot.autoconfigure.web.ServerProperties.Tomcat.Resource;
 
-//import javax.annotation.Resource;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer{
-	private static final String[] RESOURCE_LOCATIONS = {
-	        //"classpath:/static/","classpath:/resources/" ,"classpath:/resources/static/"
-			"classpath:/static/"
-	        };
-
+	
+    @Value("${spring.web.resources.static-locations}")
+    private String staticResouceLocation;
+    
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-        .addResourceHandler("/**")
-        .addResourceLocations(RESOURCE_LOCATIONS)
-        .setCachePeriod(3600)
-        .resourceChain(true)
-        .addResolver(new PathResourceResolver() { 
-        	@Override 
-        	protected Resource getResource(String resourcePath, Resource location) throws IOException {
-        		Resource requestedResource = location.createRelative(resourcePath); 
-        		return requestedResource.exists() && requestedResource.isReadable() ? 
-        				requestedResource : new ClassPathResource("/static/index.html"); 
-        		} 
-        	});
-
+        registry.addResourceHandler("/**").addResourceLocations(staticResouceLocation);
     }
+    
+//	private static final String[] RESOURCE_LOCATIONS = {
+//	        //"classpath:/static/","classpath:/resources/" ,"classpath:/resources/static/"
+//			"classpath:/static/"
+//	        };
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry
+//        .addResourceHandler("/**")
+//        .addResourceLocations(RESOURCE_LOCATIONS)
+//        .setCachePeriod(3600)
+//        .resourceChain(true)
+//        .addResolver(new PathResourceResolver() { 
+//        	@Override 
+//        	protected Resource getResource(String resourcePath, Resource location) throws IOException {
+//        		Resource requestedResource = location.createRelative(resourcePath); 
+//        		return requestedResource.exists() && requestedResource.isReadable() ? 
+//        				requestedResource : new ClassPathResource("/static/index.html"); 
+//        		} 
+//        	});
+//
+//    }
 }
