13a225712b2fe490b4d1008196cac3cec7a1b698
hyounggo <hyounggo@crossent.com>
2021-10-10 19:48:54 +0900

    commit

--- src/main/java/kr/or/uknowhat/api/framework/config/WebConfiguration.java
+++ src/main/java/kr/or/uknowhat/api/framework/config/WebConfiguration.java
@@ -1,8 +1,17 @@
 package kr.or.uknowhat.api.framework.config;
 
+import java.io.IOException;
+
+//import org.springframework.boot.autoconfigure.web.ServerProperties.Tomcat.Resource;
+
+//import javax.annotation.Resource;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
@@ -19,6 +28,14 @@ public class WebConfiguration implements WebMvcConfigurer{
         .addResourceLocations(RESOURCE_LOCATIONS)
         .setCachePeriod(3600)
         .resourceChain(true)
-        .addResolver(new PathResourceResolver());
+        .addResolver(new PathResourceResolver() { 
+        	@Override 
+        	protected Resource getResource(String resourcePath, Resource location) throws IOException {
+        		Resource requestedResource = location.createRelative(resourcePath); 
+        		return requestedResource.exists() && requestedResource.isReadable() ? 
+        				requestedResource : new ClassPathResource("/static/index.html"); 
+        		} 
+        	});
+
     }
 }
