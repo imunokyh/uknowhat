4cdab7544d13edadec8598c1e0c782f294c24d1c
hyounggo <hyounggo@crossent.com>
2021-10-10 18:58:50 +0900

    웹 설정

---
+++ src/main/java/kr/or/uknowhat/api/framework/config/WebConfiguration.java
@@ -0,0 +1,23 @@
+package kr.or.uknowhat.api.framework.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+@Configuration
+public class WebConfiguration extends WebMvcConfigurerAdapter{
+	private static final String[] RESOURCE_LOCATIONS = {
+	        "classpath:/static/","classpath:/resources/" ,"classpath:/resources/static/"
+	        };
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+        .addResourceHandler("/**")
+        .addResourceLocations(RESOURCE_LOCATIONS)
+        .setCachePeriod(3600)
+        .resourceChain(true)
+        .addResolver(new PathResourceResolver());
+    }
+}
