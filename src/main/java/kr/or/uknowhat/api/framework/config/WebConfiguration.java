a11bf46ee9c36009b1aca1e5a9bd495e76a087ca
hyounggo <hyounggo@crossent.com>
2021-10-10 22:05:56 +0900

    구성변경

--- src/main/java/kr/or/uknowhat/api/framework/config/WebConfiguration.java
+++ src/main/java/kr/or/uknowhat/api/framework/config/WebConfiguration.java
@@ -15,8 +15,8 @@ import org.springframework.web.servlet.resource.PathResourceResolver;
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer{
 	
-    @Value("${spring.web.resources.static-locations}")
-    private String staticResouceLocation;
+    //@Value("${spring.web.resources.static-locations}")
+    //private String staticResouceLocation;
     
 //    @Override
 //    public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -33,18 +33,18 @@ public class WebConfiguration implements WebMvcConfigurer{
         registry
         .addResourceHandler("/**")
         //.addResourceLocations(staticResouceLocation)
-        .addResourceLocations(RESOURCE_LOCATIONS)
-        .addResourceLocations(staticResouceLocation)
-        .setCachePeriod(3600)
-        .resourceChain(true)
-        .addResolver(new PathResourceResolver() { 
-        	@Override 
-        	protected Resource getResource(String resourcePath, Resource location) throws IOException {
-        		Resource requestedResource = location.createRelative(resourcePath); 
-        		return requestedResource.exists() && requestedResource.isReadable() ? 
-        				requestedResource : new ClassPathResource("/static/index.html"); 
-        		} 
-        	});
+        .addResourceLocations(RESOURCE_LOCATIONS);
+        //.addResourceLocations(staticResouceLocation)
+//        .setCachePeriod(3600)
+//        .resourceChain(true)
+//        .addResolver(new PathResourceResolver() { 
+//        	@Override 
+//        	protected Resource getResource(String resourcePath, Resource location) throws IOException {
+//        		Resource requestedResource = location.createRelative(resourcePath); 
+//        		return requestedResource.exists() && requestedResource.isReadable() ? 
+//        				requestedResource : new ClassPathResource("/static/index.html"); 
+//        		} 
+//        	});
 
     }
 }
